75f66996b1df2d54f9977b09bdd8fd743503665e
XBIsland <yujian0108@gmail.com>
2022-07-26 20:49:22 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      app
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </nav>
-    <router-view />
+    <div>{{ message }}</div>
+    <button @click="handleClick">click</button>
+    <HomeView
+      @custom="customEvent"
+      @click="clickHandler"
+      @dblclick.native="dblclickHandler"
+    />
+    <!-- <router-view /> -->
   </div>
 </template>
+
+<script>
+import HomeView from "./views/HomeView.vue";
+export default {
+  components: { HomeView },
+  name: "AppVue",
+  data() {
+    return {
+      visiable: false,
+      message: "initMessage",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log("handleClick", this.$data);
+    },
+    customEvent() {},
+    clickHandler() {},
+  },
+};
+</script>
 
 <style lang="stylus">
 #app
